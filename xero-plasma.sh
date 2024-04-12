882dd4faf9e4059665c82d02187383104e7c5d42
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2024-04-12 16:18:44 +0300

    Create xero-plasma.sh

---
+++ xero-plasma.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Author 	  : 	DarkXero
+# Website 	: 	https://xerolinux.xyz
+# To be used in Arch-Chroot (After installing Base packages via ArchInstall)
+##################################################################################################################
+echo
+echo "#############################################"
+echo "             KDE-Plasma Install              "
+echo "#############################################"
+echo
+echo "This is you choose how Plasma will be installed. Select option that fits your needs."
+echo
+echo "Custom option was curated by myself and only use selective if you know what you are doing."
+echo
+select lutris in "Minimal" "Full" "Custom" "Selective"; do case $lutris in Minimal) pacman -S --needed --noconfirm plasma-meta konsole kate dolphin ark plasma-workspace egl-wayland && break ;; Full) pacman -S --needed --noconfirm kf6 qt6 plasma-meta kde-applications-meta kdeconnect packagekit-qt6 kde-cli-tools kdeplasma-addons plasma-activities polkit-kde-agent flatpak-kcm bluedevil glib2 ibus kaccounts-integration kscreen libaccounts-qt plasma-nm plasma-pa scim extra-cmake-modules kaccounts-integration 
+kdoctools libibus wayland-protocols plasma-applet-window-buttons plasma-workspace appmenu-gtk-module kwayland-integration plasma5-integration xdg-desktop-portal-gtk && break ;; Custom) pacman -S --noconfirm --needed kf6 qt6 jq xmlstarlet plasma-desktop packagekit-qt6 packagekit dolphin kcron khelpcenter kio-admin ksystemlog breeze discover plasma-workspace plasma-workspace-wallpapers powerdevil plasma-nm kaccounts-integration kdeplasma-addons plasma-pa plasma-integration plasma-browser-integration plasma-wayland-protocols plasma-systemmonitor kpipewire keysmith krecorder kweather plasmatube plasma-pass ocean-sound-theme qqc2-breeze-style plasma5-integration kdeconnect kdenetwork-filesharing kget kio-extras kio-gdrive kio-zeroconf colord-kde gwenview kamera kcolorchooser kdegraphics-thumbnailers kimagemapeditor kolourpaint okular spectacle svgpart ark kate kcalc kcharselect kdebugsettings kdf kdialog keditbookmarks kfind kgpg konsole markdownpart yakuake audiotube elisa ffmpegthumbs plasmatube dolphin-plugins pim-data-exporter pim-sieve-editor emoji-font ttf-joypixels gcc-libs glibc icu kauth kbookmarks kcmutils kcodecs kcompletion kconfig kconfigwidgets kcoreaddons kcrash kdbusaddons kdeclarative kglobalaccel kguiaddons ki18n kiconthemes kio kirigami kirigami-addons kitemmodels kitemviews kjobwidgets kmenuedit knewstuff knotifications knotifyconfig kpackage krunner kservice ksvg kwidgetsaddons kwindowsystem kxmlgui libcanberra libksysguard  libplasma libx11 libxcb libxcursor libxi libxkbcommon libxkbfile plasma-activities plasma-activities-stats plasma5support polkit-kde-agent qt6-5compat qt6-base qt6-declarative qt6-wayland sdl2 solid sonnet systemsettings wayland xcb-util-keysyms xdg-user-dirs scim extra-cmake-modules intltool wayland-protocols xf86-input-libinput sddm-kcm bluedevil breeze-gtk drkonqi kde-gtk-config kdeplasma-addons kinfocenter kscreen ksshaskpass oxygen oxygen-sounds xdg-desktop-portal-kde breeze-grub flatpak-kcm falkon && break ;; Selective) pacman -S --needed kf6 qt6 plasma-meta kde-applications-meta kdeconnect packagekit-qt6 kde-cli-tools kdeplasma-addons plasma-activities polkit-kde-agent flatpak-kcm bluedevil glib2 ibus kaccounts-integration kscreen libaccounts-qt plasma-nm plasma-pa scim extra-cmake-modules kaccounts-integration 
+kdoctools libibus wayland-protocols plasma-applet-window-buttons plasma-workspace appmenu-gtk-module kwayland-integration plasma5-integration xdg-desktop-portal-gtk && break ;; *) echo "Invalid option. Please select 1, 2, 3 or 4." ;; esac done
+echo
+echo "Activating SDDM for ya..."
+echo
+systemctl enable sddm.service
+echo
+echo "#################################################"
+echo "#        Done ! Returning to main menu..        #"
+echo "#################################################"
+sleep 3
+clear && sh $0
+echo
+sleep 3
+echo "#############################################"
+echo "           Grabbing nVidia Drivers           "
+echo "#############################################"
+echo
+pacman -S --noconfirm --needed linux-headers nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland opencl-nvidia lib32-opencl-nvidia libvdpau-va-gl libvdpau
+echo
+echo "Doing necessary for Wayland shit..."
+echo
+sed -i '/^MODULES=(/ s/)$/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf && mkinitcpio -P
+sleep 3
+systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service nvidia-powerd.service
+sleep 3
+echo -e 'options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0 NVreg_DynamicPowerManagement=0x02' | tee -a /etc/modprobe.d/nvidia.conf
+echo -e 'options nvidia_drm modeset=1 fbdev=1' | tee -a /etc/modprobe.d/nvidia.conf
+sleep 3
+echo
+echo "##############################################"
+echo "               Doing Other Stuff              "
+echo "##############################################"
+echo
+echo "Adding XeroLinux Repository..."
+echo
+echo -e '\n[xerolinux]\nSigLevel = Optional TrustAll\nServer = https://repos.xerolinux.xyz/$repo/$arch' | tee -a /etc/pacman.conf
+sed -i '/^\s*#\s*\[multilib\]/,/^$/ s/^#//' /etc/pacman.conf
+echo
+echo "Installing Paru/Toolkit..."
+echo
+pacman -Syyu --noconfirm paru-bin xlapit-cli
+echo
+echo "Installing PipeWire packages..."
+echo
+pacman -S --needed --noconfirm gstreamer gst-libav gst-plugins-bad gst-plugins-base gst-plugins-ugly gst-plugins-good libdvdcss alsa-utils alsa-firmware pavucontrol lib32-pipewire-jack libpipewire pipewire-v4l2 pipewire-x11-bell pipewire-zeroconf realtime-privileges sof-firmware ffmpeg ffmpegthumbs ffnvcodec-headers
+sleep 3
+echo
+echo "Installing Bluetooth packages..."
+echo
+pacman -S --needed --noconfirm bluez bluez-utils bluez-plugins bluez-hid2hci bluez-cups bluez-libs bluez-tools
+echo
+systemctl enable bluetooth.service
+echo
+echo "Adding support for OS-Prober"
+echo
+pacman -S --noconfirm --needed os-prober
+sed -i 's/#\s*GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' '/etc/default/grub'
+os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+echo
+sleep 3
+echo
+echo "Installing your native shit..."
+echo
+pacman -S --needed --noconfirm vivaldi vivaldi-ffmpeg-codecs vivaldi-widevine meld timeshift nomachine elisa mpv gparted gnome-disk-utility wine-meta xero-fix-scripts xero-hooks
+echo
+grub-mkconfig -o /boot/grub/grub.cfg
+echo
+sleep 3
+echo "#############################################"
+echo "           Done, now exit n reboot           "
+echo "#############################################"
