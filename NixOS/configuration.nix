5a7e85415bbf24701eb7e5d60a529e97d7cc75bd
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2025-07-13 10:55:58 +0300

    Update configuration.nix

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -27,6 +27,22 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Automount Drives
+  fileSystems."/mnt/Stuffed" =
+    { device = "/dev/disk/by-uuid/0b1f92a9-cb26-4f05-8346-4059285c5088";
+      fsType = "xfs";
+    };
+
+  fileSystems."/mnt/Linux" =
+    { device = "/dev/disk/by-uuid/d19923d1-2482-4d8b-9d44-f83a75440165";
+      fsType = "ext4";
+    };
+
+  fileSystems."/mnt/Games" =
+    { device = "/dev/disk/by-uuid/8a69f985-6d4d-499b-a462-decd15f00cd1";
+      fsType = "xfs";
+    };
+
   # Set your time zone.
   time.timeZone = "Asia/Beirut";
 
@@ -167,6 +183,8 @@
     hugo
     uget
     mesa
+    unrar
+    p7zip
     cmake
     figlet
     lolcat
