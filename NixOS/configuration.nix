99ea343a0f73beb1f55eb8b08f3c1e8e874decae
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2025-07-13 12:39:20 +0300

    Update configuration.nix

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -95,9 +95,21 @@
   };
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services = {
+  displayManager ={
+      sddm = {
+       enable = true;
+       wayland = true;
+      };
+      autologin = {
+         enable = true;
+         user = "xero";    
+         }; 
+  };
+   xserver.enable = true;
+   desktopManager.plasma6.enable = true;
+   flatpak.enable = true;   
+};
 
   # Configure keymap in X11
   services.xserver.xkb = {
