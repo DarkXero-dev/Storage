9781fcdeed947e99f830e35aedb3f0eac0f91463
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2025-07-15 22:07:25 +0300

    Update configuration.nix

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -26,7 +26,7 @@
     ];
     kernelModules = [ "v4l2loopback" ];
     extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Virtual Camera" exclusive_caps=1
     '';
   };
 
